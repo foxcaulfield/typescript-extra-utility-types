c24d829957a2de90e944b8327fd10f13e9e0f19f
Maxim R <79020992+foxcaulfield@users.noreply.github.com>
2025-03-05 16:16:00 +0100

    Update utilityTypes.ts

--- utilityTypes.ts
+++ utilityTypes.ts
@@ -1,7 +1,65 @@
-type Entries<T> = {
-	[K in keyof T]: [K, T[K]];
-}[keyof T][];
+// ========================== Utility Types ==========================
 
-type Writable<T> = {
-	-readonly [K in keyof T]: T[K];
-};
+/**
+ * Properly types Object.entries and provides a helper function.
+ * 
+ * @example
+ * const obj = { a: 1, b: "test" };
+ * const entries = getEntries(obj); // ['a', 1] | ['b', "test"]
+ */
+type ObjectEntries<T> = {
+  [K in keyof T]: [K, T[K]];
+}[keyof T][];
+	
+const getEntries = <T extends Record<string, unknown>>(obj: T) => Object.entries(obj) as ObjectEntries<T>;
+
+/**
+ * Makes all properties of an object mutable.
+ * 
+ * @example
+ * interface ReadonlyObject { readonly x: number; readonly y: string; }
+ * type MutableObject = Writable<ReadonlyObject>; // { x: number; y: string; }
+ */
+type Writable<T> = { -readonly [K in keyof T]: T[K] };
+
+/**
+ * Extracts the element type from an array type.
+ * 
+ * @example
+ * const sampleArray = [1, 2, 3, "hello", true, 7, 8];
+ * type TElem = ElementOfArray<SampleArray>; // number | string | boolean
+ */
+type ElementOfArray<ArrayType extends readonly unknown[]> = 
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+/**
+ * Extracts the writable keys of an object.
+ * 
+ * @example
+ * interface TestModel { 
+ *	propOne: string; 
+ *	propTwo: string; 
+ *	readonly propThree: string; 
+ * }
+ *
+ * type TestModelWritableKeys = WritableKeys<TestModel>; // "propOne" | "propTwo"
+ */
+type WritableKeys<T> = {
+  [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>;
+}[keyof T];
+
+/**
+ * Extracts the readonly keys of an object.
+ * 
+ * @example
+ * interface TestModel { 
+ *	propOne: string; 
+ *	propTwo: string; 
+ *	readonly propThree: string; 
+ * }
+ *
+ * type TestModelReadonlyKeys = ReadonlyKeys<TestModel>; // "propThree"
+ */
+type ReadonlyKeys<T> = {
+  [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, never, P>;
+}[keyof T];
